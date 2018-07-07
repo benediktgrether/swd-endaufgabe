c1ed29382e16ce051a0269b80f11143fccacea30
Benedikt Grether <benedikt@pixelarbeit.de>
2018-07-07 20:57:03 +0200

    Add Items

--- Controls.cs
+++ Controls.cs
@@ -67,15 +67,15 @@ namespace swd_endaufgabe
                         break;
                     case "take":
                     case "t":
-                        takeItem(currentLocation);
+                        Items.takeItem(currentLocation);
                         break;
                     case "drop":
                     case "d":
-                        dropItem(currentLocation);
+                        Items.dropItem(currentLocation);
                         break;
                     case "inventory":
                     case "i":
-                        showInventory();
+                        Items.showInventory();
                         break;
                     case "look":
                     case "l":
@@ -95,46 +95,7 @@ namespace swd_endaufgabe
                 }
             }
         }
-        public static void takeItem(Location location)
-        {
-            string item = "";
-            foreach (var i in location.items)
-            {
-                item = i;
-                Chloe.inventory.Add(i);
-            }
-            location.items.Remove(item);
-        }
-
-        public static void showInventory()
-        {
-            foreach (var i in Chloe.inventory)
-            {
-                Console.WriteLine("Inventar: " + i);
-            }
-        }
-
-        public static void dropItem(Location location)
-        {
-            showInventory();
-            Console.WriteLine("Welches Item möchtest du fallen lassen?: ");
-            string drop = Console.ReadLine();
-            string item = "";
-            foreach (var i in Chloe.inventory)
-            {
-                if (drop == i)
-                {
-                    item = drop;
-                    Console.WriteLine("test");
-                    location.items.Add(item);
-                }
-            }
-            if(item != drop)
-            {
-                Console.WriteLine("Falsche Eingabe");
-            }
-            Chloe.inventory.Remove(item);
-        }
+        
     }
 
 }
--- Items.cs
+++ Items.cs
@@ -1,17 +1,49 @@
 using System;
 using System.Collections.Generic;
 
-namespace swd_projekt
+namespace swd_endaufgabe
 {
     class Items
     {
-        public string title;
-        public string description;
-
-        public Items(string _title, string _description)
+        public static void takeItem(Location location)
         {
-            title = _title;
-            description = _description;
+            string item = "";
+            foreach (var i in location.items)
+            {
+                item = i;
+                Chloe.inventory.Add(i);
+            }
+            location.items.Remove(item);
+        }
+
+        public static void showInventory()
+        {
+            foreach (var i in Chloe.inventory)
+            {
+                Console.WriteLine("Inventar: " + i);
+            }
+        }
+
+        public static void dropItem(Location location)
+        {
+            showInventory();
+            Console.WriteLine("Welches Item möchtest du fallen lassen?: ");
+            string drop = Console.ReadLine();
+            string item = "";
+            foreach (var i in Chloe.inventory)
+            {
+                if (drop == i)
+                {
+                    item = drop;
+                    Console.WriteLine("test");
+                    location.items.Add(item);
+                }
+            }
+            if (item != drop)
+            {
+                Console.WriteLine("Falsche Eingabe");
+            }
+            Chloe.inventory.Remove(item);
         }
     }
 
